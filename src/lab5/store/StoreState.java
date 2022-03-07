97d4830eb0cf7f1146ffd4bcb355160b0e93e9bd
Spazev <Linus333@telia.com>
2022-03-07 15:54:07 +0100

    Update StoreState.java

--- src/lab5/store/StoreState.java
+++ src/lab5/store/StoreState.java
@@ -2,26 +2,61 @@ package lab5.store;
 
 import lab5.general.State;
 
+import java.lang.reflect.Array;
+
 public class StoreState extends State  {   
     private CustomerQueue customerQueue;
+    private Array customers;
     private boolean open;
     private int maxCustomers;
     private int customersInStore;
     private int checkouts;
+    
+    private double openTime;
+    private double currentTime;
+
     private double minPayTime;
     private double maxPayTime;
     private double minPickTime;
     private double maxPickTime;
+    private int lambda; // customers per timme 
     
+    /**
+     * checks if the store is open/closed.
+     * @return returns a bolian depending if the store is open or not
+     */
     public boolean isOpen(){
         return open;
     }
-
+    
+    /**
+     * Closes the store.
+     */
     public void close(){
         open=false;
     }
-    public boolean space()
+    
+    public boolean space(){
+    	// kollar om det finn utrymme i affären
+    }
+    
+    /**
+     * send the current time
+     * @return the current time
+     */
+    public double getTime() {
+    	return currentTime;
+    }
+    
+    /**
+     * Updates the time.
+     * @param timeElapsed how much time has passed
+     */
+    public void updateTime(double timeElapsed) {
+    	currentTime+=timeElapsed;
+    }
 
 
-    private int lambda; // customers per timme 
+    
 }
+
