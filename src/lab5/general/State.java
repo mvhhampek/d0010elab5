11d9751adf05b06cf9070386869588e490ec69e3
mvhhampek <hampus.kamppi02@gmail.com>
2022-03-08 12:49:38 +0100

    arrivalevent

--- src/lab5/general/State.java
+++ src/lab5/general/State.java
@@ -1,10 +1,17 @@
 package lab5.general;
-
+import lab5.store.StoreState;;
 import java.util.Observable;
 
 public class State extends Observable {
     private Simulator simulator;
+    private StoreState storeState; 
     public State(Simulator s){
         simulator=s;
     }
+
+
+    public StoreState getStore(){
+        return storeState;
+    }
+
 }
--- src/lab5/store/ArrivalEvent.java
+++ src/lab5/store/ArrivalEvent.java
@@ -1,39 +1,38 @@
 package lab5.store;
 
+import lab5.general.State;
 import lab5.general.Event;
 import lab5.general.EventQueue;
 
 
 public class ArrivalEvent extends Event  {
 	private Customer customer;
-	private StoreState storeState;
+	private State state;
 	private EventQueue eventQueue;
 	private double time;
+	private StoreState storeState;
 	
 	// skapar ett nytt pick event och arrivalevent
 
-	public ArrivalEvent(StoreState storeState, EventQueue eventQueue, double time){
-		this.storeState = storeState;
+	public ArrivalEvent(State state, EventQueue eventQueue, double time){
+		this.state = state;
 		this.eventQueue = eventQueue;
 		this.time = time;
+		storeState = state.getStore();
 	}
 
 	public void execute() {
 		if(storeState.isOpen() && storeState.space()){
-			//PickEvent pick = new PickEvent();
-			//ArrivalEvent arrive = new ArrivalEvent(storeState, eventQueue);
 			eventQueue.push(new PickEvent());
-			eventQueue.push(new ArrivalEvent(storeState, eventQueue, time));
+			eventQueue.push(new ArrivalEvent(state, eventQueue, time));
 		}
 		if(storeState.isOpen() && !storeState.space()){
-			//ArrivalEvent arrive = new ArrivalEvent(storeState, eventQueue);
 			storeState.missedCustomer();
-			eventQueue.push(new ArrivalEvent(storeState, eventQueue, time));
+			eventQueue.push(new ArrivalEvent(state, eventQueue, time));
 		}
 		if(!storeState.isOpen()){
 			//inte missad kund
 		}
-
 	}
 
 	public double getTime() {
