8145057ef67301d491ad408480777faf9b4ba7d9
mvhhampek <hampus.kamppi02@gmail.com>
2022-03-08 16:56:13 +0100

    Update RunSim.java

--- src/lab5/RunSim.java
+++ src/lab5/RunSim.java
@@ -1,5 +1,19 @@
 package lab5;
+import lab5.general.State;
+
+
 
 public class RunSim {
+    public static void main(String[] args){
+        int maxCheckouts = 2;
+        int maxCustomers = 5;
+        int lambda = 1;
+        double pickMin = 0.5;
+        double pickMax = 1.0;
+        double payMin = 2.0;
+        double payMax = 3.0;
+        long seed = 1234;
 
+        
+    }
 }
