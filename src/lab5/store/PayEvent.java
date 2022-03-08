ef5769fdc28daaa39fa377c166c62e1da46dddb9
GourraG <gusedn-1@student.ltu.se>
2022-03-08 13:04:42 +0100

    anushål event bygge

--- src/lab5/store/PayEvent.java
+++ src/lab5/store/PayEvent.java
@@ -1,9 +1,23 @@
 package lab5.store;
 
+import lab5.general.State;
 import lab5.general.Event;
+import lab5.general.EventQueue;
+
 
 public class PayEvent extends Event  {
 	private Customer customer;
+	private State state;
+	private EventQueue eventQueue;
+	private double time;
+	private StoreState storeState;
+
+	public PayEvent(State state, EventQueue eventQueue, double time){
+		this.state = state;
+		this.eventQueue = eventQueue;
+		this.time = time;
+		storeState = state.getStore();
+	}
 
 	void execute() {
 
