7c215c1606ced8b6e993d19d4dc75371f3e99bce
LTUJonathan <jonjun-1@student.ltu.se>
2022-03-08 15:54:39 +0000

    CloseEvent

--- src/lab5/store/CloseEvent.java
+++ src/lab5/store/CloseEvent.java
@@ -3,13 +3,15 @@ package lab5.store;
 import lab5.general.Event;
 
 public class CloseEvent extends Event  {
-	public void execute() {
+	private StoreState storeState;
+	private StoreState time;
 
+	public void execute() {
+		storeState.close();
 	}
 
 	public double getTime() {
-
-		return 0;
+		return time.getTime();
 	}
 
 	public Customer getCustomer(){
