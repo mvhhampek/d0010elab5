b5f98f0992bf02373ba3c4a8072c8538eab82922
LTUJonathan <jonjun-1@student.ltu.se>
2022-03-09 17:01:46 +0000

    mer

--- src/lab5/Optimize.java
+++ src/lab5/Optimize.java
@@ -3,20 +3,21 @@ package lab5;
 import lab5.general.EventQueue;
 import lab5.general.Simulator;
 import lab5.general.State;
+import lab5.general.View;
 import lab5.store.CloseEvent;
 import lab5.store.EndEvent;
 import lab5.store.StartEvent;
 import lab5.store.StoreState;
 
 public class Optimize {
-    Simulator simulator;
-    StoreState storeState;
-    State state;
 
     public String metod1(int kassor){
-        new StoreState(kassor,15, 4, 0.25, 0.50, 0.25, 0.50, 133742069);
-        new Simulator(state);
+        StoreState storeState = new StoreState(kassor,15, 4, 0.5, 1.00, 0.5, 1.0, 1234);
+        View view = new View();
+        State state = new State(storeState, view);
+        Simulator simulator = new Simulator(state);
         simulator.run();
+
         String result1 = ("Av " + storeState.getMaxCustomers() + " kunder handlade " + (storeState.getMaxCustomers()-storeState.getMissedCostumers()) + " medan " + storeState.getMissedCostumers() + " missades.");
         String result2 = ("Total tid " + storeState.getMaxCheckouts() + " varit lediga: " + storeState.getLedigTid +" te." + "\n" + "Genomsnittlig ledig  kassatid: " + (storeState.getLedigTid()/storeState.getMaxCheckouts()) + " (dvs " + ((storeState.getköti()/storeState.getTime())*100) + "% av tiden från öppning tills sista kunden betalat.");
         String result3 = ("Totalt  tid " + storeState.getCustomersQueued() + " kunder tvingades  köa: " + storeState.getQueueTime() + " te." + "\n" + "Genomsnittlig kötid: " + (storeState.getQueueTime()/storeState.getCustomersQueued()) + " te.");
